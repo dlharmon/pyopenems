8f7a6b4411b81721ba26da8b6c87230335bbe15a
Darrell Harmon <darrell@harmoninstruments.com>
2015-09-06 10:36:35 -0600

    add offset, use auto naming

--- idbpf.py
+++ idbpf.py
@@ -9,7 +9,7 @@ mm = 0.001
 import numpy as np
 
 class IDBPF():
-    def __init__(self, 
+    def __init__(self,
                  em, # openems instance
                  metal_name = 'pec', # metal instance, define with openems.Metal()
                  z = [], # z position vector, z[0] = substrate bottom, z[1] = substrate top, z[2] = foil top
@@ -24,9 +24,11 @@ class IDBPF():
                  viaoffset = [0.0, 0.0], # [x,y], shift the vias to simulate drill registration error
                  via_radius = 0.15*mm, # radius of the via drills
                  via_padradius = 0.3*mm, # radius of the via pads
-                 mask_thickness = 0, # set to non-zero to enable solder mask over filter 
+                 mask_thickness = 0, # set to non-zero to enable solder mask over filter
                  inner_metal = False, # set to True to enable inner metal layers in via ring
-                 inner_metal_z = [[]]): # 
+                 inner_metal_z = [[]],
+                 offset = np.array([0.0, 0.0, 0.0]) # offset
+    ):
         self.em = em
         self.metal_name = metal_name
         self.z = z
@@ -46,6 +48,7 @@ class IDBPF():
         self.mask_thickness = mask_thickness
         self.inner_metal = inner_metal
         self.inner_metal_z = inner_metal_z
+        self.offset = offset
     def generate(self):
         # fingers
         y = -0.5*self.space[-1:][0]
@@ -54,93 +57,111 @@ class IDBPF():
         for i in range(len(self.space))[::-1]: # reverse order
             x1 = 0.5 * (self.ring_ox + self.ring_ix)
             x2 = self.ring_ix - self.rl[i]
-            fingername = 'r{}'.format(i)
             y += self.space[i]
             start = [x1, y, self.z[1]];
             y += self.rw[i]
             stop  = [x2, y, self.z[2]];
-            box = openems.Box(self.em, fingername, self.metal_name, self.priority, start, stop, padname = '2')
+            box = openems.Box(self.em, None, self.metal_name, self.priority, start, stop, padname = '2')
             box.mirror(mirrorstring[mirror])
             mirror = not mirror
-            box2 = box.duplicate(fingername + 'm')
+            box2 = box.duplicate()
             box2.mirror('xy')
             if i == 0:
                 box.padname = '1'
                 box2.padname = '3'
+            box.offset(self.offset)
+            box2.offset(self.offset)
         # feed lines
         mirror = not mirror
         y -= 0.5*self.rw[0] # center of line
         start = [self.ring_ix, y + 0.5*self.feedwidth, self.z[1]]
         stop  = [self.ring_ox - self.portlength, y - 0.5*self.feedwidth, self.z[2]]
-        box = openems.Box(self.em, 'pext1', self.metal_name, self.priority, start, stop, padname = '1')
+        box = openems.Box(self.em, None, self.metal_name, self.priority, start, stop, padname = '1')
         box.mirror(mirrorstring[mirror])
-        box2 = box.duplicate('pext2')
+        box2 = box.duplicate()
         box2.mirror('xy')
         box2.padname = '3'
+        box.offset(self.offset)
+        box2.offset(self.offset)
         # ports
         start = [self.ring_ox, y + 0.5*self.feedwidth, self.z[1]]
         stop  = [self.ring_ox - self.portlength, y - 0.5*self.feedwidth, self.z[2]]
         p = openems.Port(self.em, start, stop, direction='x', z=50)
         p.mirror(mirrorstring[mirror])
-        p.duplicate().mirror('xy')
+        p2 = p.duplicate().mirror('xy')
+        p.offset(self.offset)
+        p2.offset(self.offset)
         # metal ring (sides)
         y1 = y + 0.5*self.feedwidth + self.feedgap
         y2 = y1 + self.ring_y_width
         start = [self.ring_ox, y1, self.z[1]]
         stop  = [-1.0 * self.ring_ox, y2, self.z[2]]
-        box = openems.Box(self.em, 'ring1', self.metal_name, self.priority, start, stop, padname = '2')
+        box = openems.Box(self.em, None, self.metal_name, self.priority, start, stop, padname = '2')
         box.mirror(mirrorstring[mirror])
-        box.duplicate('ring2').mirror('xy')
+        box2 = box.duplicate().mirror('xy')
+        box.offset(self.offset)
+        box2.offset(self.offset)
         # metal ring (sides)
         y3 = y - (0.5*self.feedwidth + self.feedgap)
         start = [self.ring_ox, y3, self.z[1]]
         stop  = [self.ring_ix, -1.0*y2, self.z[2]]
-        box = openems.Box(self.em, 'ring3', self.metal_name, self.priority, start, stop, padname = '2')
+        box = openems.Box(self.em, None, self.metal_name, self.priority, start, stop, padname = '2')
         box.mirror(mirrorstring[mirror])
-        box.duplicate('ring4').mirror('xy')
+        box2 = box.duplicate().mirror('xy')
+        box.offset(self.offset)
+        box2.offset(self.offset)
         # metal - inner ends
         if self.inner_metal:
-            imname = 0
             for z in self.inner_metal_z:
                 start = [self.ring_ox, y1, z[0]]
                 stop  = [-1.0 * self.ring_ox, y2, z[1]]
-                box = openems.Box(self.em, 'mi{}'.format(imname), self.metal_name, self.priority, start, stop, padname = None)
-                box.duplicate('mi{}'.format(imname+1)).mirror('xy')
+                box = openems.Box(self.em, None, self.metal_name, self.priority, start, stop, padname = None)
+                box2 = box.duplicate().mirror('xy')
+                box.offset(self.offset)
+                box2.offset(self.offset)
                 start = [self.ring_ox, y2, z[0]]
                 stop  = [self.ring_ix, -1.0*y2, z[1]]
-                box = openems.Box(self.em, 'mi{}'.format(imname+2), self.metal_name, self.priority, start, stop, padname = None)
-                box.duplicate('mi{}'.format(imname+3)).mirror('xy')
-                imname += 4
+                box = openems.Box(self.em, None, self.metal_name, self.priority, start, stop, padname = None)
+                box2 = box.duplicate().mirror('xy')
+                box.offset(self.offset)
+                box2.offset(self.offset)
         # substrate
         start = np.array([self.ring_ox, y2, self.z[0]])
-        stop  = openems.mirror(start, 'xy') 
+        stop  = openems.mirror(start, 'xy')
         stop[2] = self.z[1]
-        openems.Box(self.em, 'sub1', 'sub', 1, start, stop);
+        sub = openems.Box(self.em, None, 'sub', 1, start, stop);
+        sub.offset(self.offset)
         # mask
         if self.mask_thickness > 0.0:
             start = np.array([self.ring_ox, y2, self.z[1]])
-            stop  = openems.mirror(start, 'xy') 
+            stop  = openems.mirror(start, 'xy')
             stop[2] += self.mask_thickness
-            openems.Box(self.em, 'mask', 'mask', 1, start, stop);
+            openems.Box(self.em, None, 'mask', 1, start, stop);
         # vias (along y)
         via_z = [[self.z[0], self.z[2]]]#, [self.z[1], self.z[2]]]
         y_start = y3 - self.via_padradius
         y_stop = -1.0* y1 - self.via_padradius
         x = self.ring_ix + self.via_padradius
+        if not self.via_radius:
+            return
         n_via = 0
         n_vias = 1 + np.floor(np.abs(y_start-y_stop)/(2.0*self.via_padradius))
         for y in np.linspace(y_start, y_stop, n_vias):
-            v = openems.Via(self.em, 'via{}'.format(n_via), 'pec', 2, x=x, y=y, z=via_z, drillradius=self.via_radius, padradius=self.via_padradius, padname='2')
+            v = openems.Via(self.em, None, 'pec', 2, x=x, y=y, z=via_z, drillradius=self.via_radius, padradius=self.via_padradius, padname='2')
             v.mirror(mirrorstring[mirror])
-            v.duplicate('via{}'.format(n_via+1)).mirror('xy')
+            v2 = v.duplicate().mirror('xy')
             n_via += 2
+            v.offset(self.offset)
+            v2.offset(self.offset)
         # vias (along x)
         y = y_stop
         x_start = self.ring_ix + self.via_padradius
         x_stop = self.via_padradius - self.ring_ox
         n_vias = 1 + np.floor(np.abs(x_start-x_stop)/(2.0*self.via_padradius))
         for x in np.linspace(x_start, x_stop, n_vias)[1:]:
-            v = openems.Via(self.em, 'via{}'.format(n_via), 'pec', 2, x=x, y=y, z=via_z, drillradius=self.via_radius, padradius=self.via_padradius, padname='2')
+            v = openems.Via(self.em, None, 'pec', 2, x=x, y=y, z=via_z, drillradius=self.via_radius, padradius=self.via_padradius, padname='2')
             v.mirror(mirrorstring[mirror])
-            v.duplicate('via{}'.format(n_via+1)).mirror('xy')
+            v2 = v.duplicate().mirror('xy')
             n_via += 2
+            v.offset(self.offset)
+            v2.offset(self.offset)
